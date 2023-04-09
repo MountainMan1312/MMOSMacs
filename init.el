8463bcd2cc93b961e771e26761109eb047070bd7
Mountain Man <mmosmacs-dev@anu.tgwil.net>
2023-04-09 19:30:24 -0400

    Disable backups, auto-saves, and lockfiles
    I hate seeing a bunch of `FILES~` and `#FILES#` laying around
    everywhere. This disables them completely (I think).

--- init.el
+++ init.el
@@ -2,3 +2,42 @@
 ;;; init.el
 ;;
 ;; Main configuration file for MMOSMacs.
+
+
+;; ---------------------------------------------------------------------
+;;; File Management
+;; ----------------
+;; Everything to do with file or directory management goes here.
+;; ---------------------------------------------------------------------
+
+;; ---------------------------------
+;; Disable backups
+;; ---------------
+;; By default, Emacs saves backup
+;; files, adding a `~' to the name.
+;; Stop saving them.
+;; ---------------------------------
+
+(setq backup-inhibited t)
+
+
+;; ---------------------------------
+;; Disable auto-saves
+;; ------------------
+;; Auto-saves are the files with
+;; hashes before and after the name.
+;; They should not exist.
+;; ---------------------------------
+
+(setq auto-save-default nil)
+
+
+;; ---------------------------------
+;; Disable lockfiles
+;; -----------------
+;; Sometimes I want to edit a locked
+;; file. I can handle myself, let me
+;; do what I want.
+;; ---------------------------------
+
+(setq create-lockfiles nil)
