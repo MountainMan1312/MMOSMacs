3255fe42431e28c12ee794d7e7fe673d3f65f5c3
Mountain Man <mmosmacs-dev@anu.tgwil.net>
2023-04-09 16:48:08 -0400

    Create `early-init.el` and `init.el` files

---
+++ early-init.el
@@ -0,0 +1,6 @@
+;;; -*- lexical-binding: t; -*-
+;;; init.el
+;;
+;; Early Init file for MMOSMacs. This is loaded before the GUI and
+;; package system are initialized. This should contain only what
+;; absolutely must be in Early Init.
---
+++ init.el
@@ -0,0 +1,4 @@
+;;; -*- lexical-binding: t; -*-
+;;; init.el
+;;
+;; Main configuration file for MMOSMacs.
